2858b48d97161ca7682b70dffc9495608c37aca7
vpsitnik <sitnik.vp@gazprom-neft.ru>
2023-04-18 00:07:40 +0300

    add whitespaces

--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -19,8 +19,8 @@ func TestUnpack(t *testing.T) {
 		{input: "a0b2c5d0", expected: "bbccccc"},
 		{input: "d\n5abc", expected: "d\n\n\n\n\nabc"},
 		// uncomment if task with asterisk completed
-		//{input: `qwe\4\5`, expected: `qwe45`},
-		//{input: `qwe\45`, expected: `qwe44444`},
+		// {input: `qwe\4\5`, expected: `qwe45`},
+		// {input: `qwe\45`, expected: `qwe44444`},
 		// {input: `qwe\\5`, expected: `qwe\\\\\`},
 		// {input: `qwe\\\3`, expected: `qwe\3`},
 	}
