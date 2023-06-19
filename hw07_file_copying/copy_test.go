039c4bc6ad96abb2009a3c227416b7ccf2f3248a
testUser <testuser@unknown.local>
2023-06-19 14:21:02 +0300

    first unit test: add compare hash files

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,58 @@
 package main
 
-import "testing"
+import (
+	"crypto/md5"
+	"io"
+	"os"
+	"testing"
 
-func TestCopy(_ *testing.T) {
-	// Place your code here.
+	"github.com/stretchr/testify/require"
+)
+
+func TestCopy(t *testing.T) {
+	const (
+		input  = "./testdata/input.txt"
+		output = "./out.txt"
+	)
+
+	tests := []struct {
+		validOut string
+		offset   int64
+		limit    int64
+	}{
+		{"./testdata/out_offset0_limit0.txt", 0, 0},
+		{"./testdata/out_offset0_limit10.txt", 0, 10},
+		{"./testdata/out_offset0_limit1000.txt", 0, 1000},
+		{"./testdata/out_offset0_limit10000.txt", 0, 10000},
+		{"./testdata/out_offset100_limit1000.txt", 100, 1000},
+		{"./testdata/out_offset6000_limit1000.txt", 6000, 1000},
+	}
+
+	t.Run("Compare input and output files", func(t *testing.T) {
+		for _, tc := range tests {
+			err := Copy(input, output, tc.offset, tc.limit)
+			require.NoError(t, err)
+			defer os.Remove(output)
+
+			hOutput := md5.New()
+			hValidOut := md5.New()
+
+			fileOutput, err := os.Open(output)
+			require.NoError(t, err)
+			defer fileOutput.Close()
+
+			fileValid, err := os.Open(tc.validOut)
+			require.NoError(t, err)
+			defer fileValid.Close()
+
+			// get hashes
+			_, err = io.Copy(hOutput, fileOutput)
+			require.NoError(t, err)
+
+			_, err = io.Copy(hValidOut, fileValid)
+			require.NoError(t, err)
+
+			require.Equal(t, hOutput, hValidOut)
+		}
+	})
 }
--- hw07_file_copying/go.mod
+++ hw07_file_copying/go.mod
@@ -6,10 +6,14 @@ require github.com/cheggaaa/pb/v3 v3.1.2
 
 require (
 	github.com/VividCortex/ewma v1.2.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fatih/color v1.14.1 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/mattn/go-runewidth v0.0.12 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
 	golang.org/x/sys v0.5.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
