2b7badf878ded9ec2f8251a8bc9c5e99d2c506e8
Vadim <vsitnik72@gmail.com>
2023-07-02 20:10:38 +0300

    hw08: add unit tests for env_reader

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,69 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
 
-func TestReadDir(_ *testing.T) {
-	// Place your code here
+	"github.com/stretchr/testify/require"
+)
+
+func TestReadDir(t *testing.T) {
+	const dataPath = "./testdata/env"
+
+	expectedMap := Environment{
+		"BAR":   {Value: "bar", NeedRemove: false},
+		"EMPTY": {Value: "", NeedRemove: false},
+		"FOO": {Value: `   foo
+with new line`, NeedRemove: false},
+		"HELLO": {Value: "\"hello\"", NeedRemove: false},
+		"UNSET": {Value: "", NeedRemove: true},
+	}
+
+	t.Run("Case equal compare testdata for ReadDir", func(t *testing.T) {
+		envResult, err := ReadDir(dataPath)
+		require.NoError(t, err)
+		require.Equal(t, expectedMap, envResult)
+	})
+
+	t.Run("Case set and unset environments", func(t *testing.T) {
+		ProcessEnv(expectedMap)
+
+		for envName, envValue := range expectedMap {
+			if !envValue.NeedRemove {
+				require.Equal(t, envValue.Value, os.Getenv(envName))
+			} else {
+				_, ok := os.LookupEnv(envName)
+				require.False(t, ok)
+			}
+		}
+	})
+
+	t.Run("Case overwrite environments", func(t *testing.T) {
+		envKey := "MYBAR"
+		envMyVal := "mybarvalue123"
+		expected := Environment{envKey: {Value: "12345", NeedRemove: false}}
+
+		os.Setenv(envKey, envMyVal)
+		require.Equal(t, envMyVal, os.Getenv(envKey))
+
+		ProcessEnv(expected)
+
+		require.NotEqual(t, envMyVal, os.Getenv(envKey))
+		require.Equal(t, expected[envKey].Value, os.Getenv(envKey))
+	})
+
+	t.Run("Case with uncorrect environment", func(t *testing.T) {
+		envKey := "MYBAR="
+		expected := Environment{envKey: {Value: "12345", NeedRemove: false}}
+
+		ProcessEnv(expected)
+
+		_, ok := os.LookupEnv(envKey)
+		require.False(t, ok)
+	})
+
+	t.Run("Negative case with uncorrect dir path", func(t *testing.T) {
+		_, err := ReadDir("/tmp123_nonexisting")
+		require.Error(t, err)
+	})
 }
