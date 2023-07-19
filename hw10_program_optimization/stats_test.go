71f328e0c5bcfcdf73b3e513af8257f00c736955
Vadim <vsitnik72@gmail.com>
2023-07-19 17:47:15 +0300

    hw10: add new cases for unit test

--- hw10_program_optimization/stats_test.go
+++ hw10_program_optimization/stats_test.go
@@ -32,6 +32,18 @@ func TestGetDomainStat(t *testing.T) {
 		require.Equal(t, DomainStat{"browsedrive.gov": 1}, result)
 	})
 
+	t.Run("find 'net'", func(t *testing.T) {
+		result, err := GetDomainStat(bytes.NewBufferString(data), "net")
+		require.NoError(t, err)
+		require.Equal(t, DomainStat{"teklist.net": 1}, result)
+	})
+
+	t.Run("find 'com'", func(t *testing.T) {
+		result, err := GetDomainStat(bytes.NewBufferString(data), "com")
+		require.NoError(t, err)
+		require.Equal(t, DomainStat{"browsecat.com": 2, "linktype.com": 1}, result)
+	})
+
 	t.Run("find 'unknown'", func(t *testing.T) {
 		result, err := GetDomainStat(bytes.NewBufferString(data), "unknown")
 		require.NoError(t, err)
